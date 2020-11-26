e936e5d60d2270acbdccdfb3b6190709f16ae546
peaky76 <robertjamespeacock@gmail.com>
2020-11-26 22:52:49 +0000

    basic game complete

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-const square = document.querySelectorAll('.square')
-const mole = document.querySelectorAll('.mole')
-const timeLeft = document.querySelectorAll('#time-left')
-let score = document.querySelectorAll('#score')
+const square = document.querySelectorAll('.square');
+const mole = document.querySelectorAll('.mole');
+const timeLeft = document.querySelector('#time-left');
+let score = document.querySelector('#score');
 
-let result = 0
-let currentTime = timeLeft.textContent
+let result = 0;
+let currentTime = timeLeft.textContent;
 
 function randomSquare() {
     square.forEach(className => {
-        className.classList.remove('mole')
+        className.classList.remove('mole');
     })
-    let randomPosition = square[Math.floor(Math.random() * 9)]
-    randomPosition.classList.add('mole')
+    let randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add('mole');
 
     //assign the id of the randomPosition to hitPosition for later use
-    hitPosition = randomPosition.id
+    hitPosition = randomPosition.id;
 }
 
 square.forEach(id => {
     id.addEventListener('mouseup', () => {
         if (id.id === hitPosition) {
-            result = result + 1
-            score.textContent = result
+            result = result + 1;
+            score.textContent = result;
         }
-    })
+    });
 })
 
 function moveMole() {
-    let timerId = null
-    timerId = setInterval(randomSquare, 1000)
+    let timerId = null;
+    timerId = setInterval(randomSquare, 1000);
 }
 
-moveMole()
+moveMole();
 
 function countDown() {
-    currentTime--
-    timeLeft.textContent = currentTime
+    currentTime--;
+    timeLeft.textContent = currentTime;
 
     if (currentTime === 0) {
-        clearInterval(timerId)
-        alert('GAME OVER! Your final score is' + result)
+        clearInterval(timerId);
+        alert('GAME OVER! Your final score is ' + result);
     }
 }
 
-let timerId = setInterval(countDown, 1000)
+let timerId = setInterval(countDown, 1000);
