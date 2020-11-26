e0368a54fe1ae81f21b9c85f5abcdc66d94a4ee1
peaky76 <robertjamespeacock@gmail.com>
2020-11-26 19:44:49 +0000

    js variables created

---
+++ app.js
@@ -0,0 +1,4 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelectorAll('#time-left')
+let score = document.querySelectorAll('#score')
